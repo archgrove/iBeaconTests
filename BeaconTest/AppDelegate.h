54d1db215d6e7ad472abde2015790b36132e2c92
Adam Wright <adamw@archgrove.co.uk>
2014-02-10 12:36:27 +0000

    Initial Commit

---
+++ BeaconTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeaconTest
+//
+//  Created by Adam Wright on 10/02/2014.
+//  Copyright (c) 2014 Adam Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeaconTest/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  BeaconTest
+//
+//  Created by Adam Wright on 10/02/2014.
+//  Copyright (c) 2014 Adam Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
